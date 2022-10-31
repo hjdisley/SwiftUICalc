7aa21f59e384eafaad3d17da76dce3c42e3ea021
Hayden Disley <hayden@pc-2-214.customer.ask4.lan>
2022-10-31 21:37:06 +0000

    initial commit

---
+++ calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Hayden Disley on 31/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
